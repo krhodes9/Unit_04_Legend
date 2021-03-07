410d8eec92f77e77057c5b72fe5e28de3aefb61f
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-03-06 22:48:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,69 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
-
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-90, 38] // longitude, latitude
-        });
+require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Legend",
+      "esri/widgets/LayerList",
+	    "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Legend, LayerList, Home) {
+    
+ 
+      
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
       });
+      
+      var camera = new Camera({
+        position: [
+           -71.060217,// lon
+          42.382655, // lat
+          2500// elevation in meters
+        ],
+        tilt:45,
+        heading: 180
+      })
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        camera: camera
+    });
+	
+view.when(function() {
+	
+          // get the first layer in the collection of operational layers in the WebMap
+          // when the resources in the MapView have loaded.
+        var featureLayer =    scene.layers.getItemAt(1);
+
+        var legend = new Legend({
+          view: view,
+          layerInfos: [{
+            layer: featureLayer,
+            title: "Major project buildings"
+          }]
+        });
+      
+   view.ui.add(legend, "bottom-right");
+   });  
+      
+      var layerList = new LayerList({
+  view: view
+});
+      view.ui.add(layerList, "bottom-right");    
+      
+      
+	var homeBtn = new Home({
+        view: view
+      });
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+
+
+    });
+
